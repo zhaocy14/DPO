ff47a2227b7e63d6586e9c48930d50db2bd094c9
Zhao Chongyu <522653608@qq.com>
2025-09-16 11:21:39 +0800

    update key layer training

--- Training/KeyLayerDPOTraining.py
+++ Training/KeyLayerDPOTraining.py
@@ -41,7 +41,7 @@ CONFIG = {
 
     "batch_size": 1,
     "epochs": 10,
-    "lr": 1e-4,
+    "lr": 8e-5,
     "num_candidates": 5,  # 单组候选动作数量
     "sampling_workers": 2,
     "max_train_samples_per_epoch": 100,
